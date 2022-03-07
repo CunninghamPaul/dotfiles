e6c89b2c692dcbad05e64543b4aa734f275fb656
CunninghamPaul <67686243+CunninghamPaul@users.noreply.github.com>
2022-03-07 22:21:02 +0000

    added zsh-common to installed packages

--- install-ohmyzsh-deb.sh
+++ install-ohmyzsh-deb.sh
@@ -3,7 +3,7 @@
 #Install Oh My ZSH and plugins for Debian/Ubuntu
 
 sudo apt update
-sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting -y
+sudo apt install zsh zsh-common zsh-autosuggestions zsh-syntax-highlighting -y
 
 #Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
