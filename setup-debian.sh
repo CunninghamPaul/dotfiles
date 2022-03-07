6e3d1a620cccb8890a2fa02838c1c14c6dc8f21c
Paul Cunningham <mail@paulcunningham.uk>
2022-03-07 21:24:36 +0000

    added Debian Linux versions

---
+++ setup-debian.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Setup dotfiles for Debian based Linux
+#Requires oh-my-zsh and plugins see install-ohmy-deb.sh
+
+#Backup any existing dotfiles
+for i in ~/.zshrc ~/.nanorc ~/.tmux.conf
+do
+    if test -f $i; then
+        mv $i $i.dfbak
+        echo $i
+    fi
+done
+
+#Create Symbolic Links
+ln -s ~/dotfiles/.zshrc-deb ~/.zshrc
+ln -s ~/dotfiles/.nanorc-deb ~/.nanorc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
