8907ec817082b9529fbf30d3799ac39186412326
Paul Cunningham <paulcunningham@Pauls-Air.minet.uk>
2023-03-18 11:44:50 +0000

    added updateauthkeys.sh

---
+++ updateauthkeys.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+me="$(whoami)"
+
+read -r -p "Are you sure you want to overwrite authorized_keys for ${me} ? y/n" choice
+
+case $choice in
+      [yY])
+            echo "Updating ~/.ssh/authorized_keys for user ${me}"
+            cat pubkeys > ~/.ssh/authorized_keys
+            ;;
+      [nN])
+            echo 'Exiting'
+            exit
+            ;;
+      *)
+            echo 'Invalid input...'
+            echo 'Exiting'
+            exit
+            ;;
+esac
+
